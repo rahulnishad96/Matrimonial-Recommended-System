bc30ebd1c9aabf5630aeb8eb536618fa4bc38382
rahulnishad96 <rahulnishadhandia@gmail.com>
2021-06-21 02:15:12 +0530

    updated full page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,10 +9,10 @@ function HomePage() {
     return (
         <div>
             {/* <img className="homepage-img"src="https://img2.sangam.com/assets/home/HomePage_Marathi.jpg" alt="homepageImage"></img> */}
-            <section className="title-section">
+            <section className="title-section" id="title">
                 <p className="title">...because a marriage is between two families</p>
             </section>
-            <section className="form-section">
+            <section className="form-section" id="form">
                 <p className="heading">Matches Within Your Community</p>
                 <p className="explain">Genuine Profiles | Safe & Secure | Detailed Family Information</p>
                 <div className="form-div">
@@ -55,15 +55,45 @@ function HomePage() {
                     </div>
                 </div>
             </section>
+            
+            <section className="story">
+                <h1 className="heading">Our Success story</h1>
+                <div className="why-heading-border"></div>
+                <div className="card-box">
+                    <div className="card">
+                        <img src="https://previews.123rf.com/images/dragonimages/dragonimages1907/dragonimages190701968/128303176-portrait-of-happy-young-indian-couple-with-opened-book-and-glass-of-lemonade-looking-at-camera.jpg" alt="image"></img>
+                        <p>Riya and Shubham</p>
+                        <a href="#" target="_blank">Read more</a>
+                    </div>
+                    <div className="card">
+                        <img src="https://st.depositphotos.com/1011643/4431/i/950/depositphotos_44310803-stock-photo-beautiful-young-indian-couple.jpg" alt="image"></img>
+                        <p>Bhavana and Deepak</p>
+                        <a href="#" target="_blank">Read more</a>
+                    </div>
+                    <div className="card">
+                        <img src="https://s3.envato.com/files/288644194/_8532770.jpg" alt="image"></img>
+                        <p>Rohit and Aruhi</p>
+                        <a href="#" target="_blank">Read more</a>
+                    </div>
+                </div>
+            </section>
+
             <section className="about-us">
                 <p className="why-heading">About Us</p>
                 <div className="why-heading-border"></div>
                 <p className="explain">This cite is a matchmaking service created for parents who are looking for a life partner for their loved ones. Unlike other Matrimonial services, we focus on providing detailed family and background information to help you take the next step with confidence. With over 80+ community sites, you can find a match from your own community. Sangam is part of Shaadi.com (sometimes mis-spelt as Shadi), the World's No. 1 Matchmaking service.</p>
             </section>
-            <section className="story">
-                <h1 className="heading">Our Success story</h1>
-                {/* <StoryItem/>  */}
+
+            <section className="community">
+                <p className="heading">Community Matchmaking, Trusted by Parents</p>
+                <a href="#title" className="btn">show matches</a>
             </section>
+
+            <footer className="footer">
+                <p>© Sangam, Community Matchmaking Trusted By Parents TM</p>
+                <p>Passionately created by <a href="#" target="_blank">Rahul Nishad</a></p>
+                <p>Version 0.1.72</p>
+            </footer>
         </div>
     )
 }
